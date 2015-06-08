c4abc9491912e08f65543084f7ca6b0339bb2ff4
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-08 14:25:23 -0500

    Adds module phone_number_formatter
    Basic functionality to manipulate strings
    Formatting for phone numbers

---
+++ phone_number_formatter.rb
@@ -0,0 +1,83 @@
+module PhoneNumberFormatter
+  
+  # returns a number formatted as a phone number with dashes
+  #   => 123-456-7890
+  #
+  # number - number or string
+  #
+  def self.return_phone_num_with_dashes(number)
+    num = return_num_as_string(number)
+    if each_char_is_a_num?(number)
+      if phone_num_right_length?(number)
+        join_phone_num_array_by_dashes(convert_string_to_phone_num_array(num))
+      else
+        not_right_num_digits
+      end
+    else
+      not_a_num_error
+    end
+  end
+  
+  def join_phone_num_array_by_dashes(array)
+    array.join('-')
+  end
+  
+  
+  # returns an Array of size 3 in phone number format
+  # => 1234567890 --> [123, 456, 7890] corresponding to area code, first three, last four digits in a phone number
+  #
+  # string - String
+  #
+  # returns Array
+  def convert_string_to_phone_num_array(string)
+    [string[0...2], string[3...5], string[6...9]]
+  end
+  
+  # returns input as a String
+  #
+  # num could be anything but probably a FixedNum
+  #
+  # returns String
+  def return_num_as_string(num)
+    num.to_s
+  end
+  
+  # checks whether phone number is the right length
+  #
+  # string - String
+  #
+  # returns boolean
+  def phone_num_right_length?(string)
+    string.length == 10
+  end
+  
+  # checks whether each char in the string is an Integer
+  #
+  # string - String
+  #
+  # returns boolean
+  def each_char_is_a_num?(string)
+    is_num = true
+    string.each_byte do |c|
+      unless ascii_of_nums.include?(c)
+        is_num = false
+      end
+    end
+    is_num
+  end
+  
+  # Array of the ascii numbers of number characters
+  def ascii_of_nums
+    (48...57)
+  end
+  # a string saying it wasn't a number
+  def not_a_num_error
+    "Not a number"
+  end
+  
+  # a string saying it wasn't the correct number of digits
+  def not_right_num_digits
+    "Not the right number of digits."
+  end
+  
+end
