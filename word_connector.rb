faf0f72ccbc1390f2900a3fda47cff33b2012644
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-09 14:40:53 -0500

    Made WordConnector more elegant and added in Oxford comma

--- word_connector.rb
+++ word_connector.rb
@@ -1,27 +1,20 @@
 module WordConnector
   
   # takes an array and makes it into an oxford sentence element
-  #   ["apples", "oranges", "grapes"] => "apples, oranges, and grapes"
+  #   ["apples", "oranges", "grapes"] => "apples, oranges and grapes"
   #
   # array - Array
   #
   # returns string
   def self.join_this_array_as_a_sentence(array)
-    sentence = ""
-    array.each_with_index do |element, index|
-      case index
-      # for last element, remove comma and add 'and'
-      when array.length - 1  
-        sentence += " and #{element}"
-      # for first element, start the sentence
-      when 0
-        sentence = "#{element}"
-      # for all other elements, use a comma first
-      else
-        sentence += ", #{element}"
-      end
+    case array.length
+    when 1
+      sentence = array[0]
+    when 2
+      sentence = "#{array[0]} and #{array[1]}"
+    else 
+      sentence = array[0..-2].join(", ") + ", and " + array[-1]
     end
-    sentence
   end
 
   
