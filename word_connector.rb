f887f7db9905646eeade7e458773a8572751c5dd
Gwendolyn Holley <gmfholley@gmail.com>
2015-06-10 12:00:09 -0500

    Updates to use active_support

--- word_connector.rb
+++ word_connector.rb
@@ -1,3 +1,6 @@
+require "active_support"
+require "active_support/core_ext/array/conversions.rb"
+
 module WordConnector
   
   # takes an array and makes it into an oxford sentence element
@@ -7,14 +10,7 @@ module WordConnector
   #
   # returns string
   def self.join_this_array_as_a_sentence(array)
-    case array.length
-    when 1
-      sentence = array[0]
-    when 2
-      sentence = "#{array[0]} and #{array[1]}"
-    else 
-      sentence = array[0..-2].join(", ") + ", and " + array[-1]
-    end
+    array.to_sentence
   end
 
   
